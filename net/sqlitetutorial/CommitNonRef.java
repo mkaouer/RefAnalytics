4884f1d54022ca26b8fed3ef3e84fe6f5bd8f641
mkaouer <mmkaouer@umich.edu>
2020-02-17 00:46:34 -0500

    Updated source code to proper output project name in output files

---
+++ net/sqlitetutorial/CommitNonRef.java
@@ -0,0 +1,18 @@
+package net.sqlitetutorial;
+
+public class CommitNonRef {
+
+	String Name;
+	String CommitSHA;
+	String Message;
+	
+	
+	public CommitNonRef ()
+	{
+		Name = new String();
+		CommitSHA = new String();
+		Message = new String();
+	}
+	 
+	
+}
---
+++ net/sqlitetutorial/CommitRef.java
@@ -0,0 +1,18 @@
+package net.sqlitetutorial;
+
+public class CommitRef {
+
+	String Name;
+	String CommitSHA;
+	String Message;
+	
+	
+	public CommitRef ()
+	{
+		Name = new String();
+		CommitSHA = new String();
+		Message = new String();
+	}
+	 
+	
+}
